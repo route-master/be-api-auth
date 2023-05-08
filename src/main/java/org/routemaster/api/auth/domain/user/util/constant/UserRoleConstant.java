9cd0e491b673223ab0419ad9b158b6e79a25c3a1
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-05-08 20:20:00 +0900

    Feat: Add Basic User's Register constants

---
+++ src/main/java/org/routemaster/api/auth/domain/user/util/constant/UserRoleConstant.java
@@ -0,0 +1,7 @@
+package org.routemaster.api.auth.domain.user.util.constant;
+
+public class UserRoleConstant {
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+}
---
+++ src/main/java/org/routemaster/api/auth/global/constant/MilliSecondConstant.java
@@ -0,0 +1,9 @@
+package org.routemaster.api.auth.global.constant;
+
+public class MilliSecondConstant {
+
+    public static final long SECOND = 1000L;
+    public static final long MINUTE = 60 * SECOND;
+    public static final long YOUR = 60 * MINUTE;
+    public static final long DAY = 24 * YOUR;
+}
