a67e866f4d490155d35b7b05e819b1b767f2dc3a
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-07-15 13:51:34 +0900

    #1 - Feat: Delete Base User's Classes

--- src/main/java/org/routemaster/api/auth/domain/user/util/constant/UserTimeConstant.java
+++
@@ -1,9 +0,0 @@
-package org.routemaster.api.auth.domain.user.util.constant;
-
-import org.routemaster.api.auth.global.constant.MilliSecondConstant;
-
-public class UserTimeConstant {
-    public static final long ACCOUNT_EXPIRATION_DURATION = 300 * MilliSecondConstant.DAY;
-    public static final long CREDENTIALS_EXPIRATION_DURATION = 90 * MilliSecondConstant.DAY;
-    public static final long ACCOUNT_READY_EXPIRATION_DURATION = 7 * MilliSecondConstant.DAY;
-}
