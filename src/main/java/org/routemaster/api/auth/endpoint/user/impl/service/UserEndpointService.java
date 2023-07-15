f7b8b524566ff4eae2b1ca7c9b39a857c5c5211c
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-07-15 13:57:22 +0900

    #1 - Feat: Add User Classes

---
+++ src/main/java/org/routemaster/api/auth/endpoint/user/impl/service/UserEndpointService.java
@@ -0,0 +1,9 @@
+package org.routemaster.api.auth.endpoint.user.impl.service;
+
+import org.routemaster.api.auth.domain.user.jwt.impl.data.UserJwtPayload;
+
+public interface UserEndpointService {
+
+    void logout(UserJwtPayload payload);
+    void delete(UserJwtPayload payload);
+}
