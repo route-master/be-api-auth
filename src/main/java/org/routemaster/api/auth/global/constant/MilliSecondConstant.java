260542eb18f6e19ec95db0a3ad07ab6b7901051f
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-07-15 14:00:40 +0900

    #1 - Feat: Delete Useless Constant

--- src/main/java/org/routemaster/api/auth/global/constant/MilliSecondConstant.java
+++
@@ -1,9 +0,0 @@
-package org.routemaster.api.auth.global.constant;
-
-public class MilliSecondConstant {
-
-    public static final long SECOND = 1000L;
-    public static final long MINUTE = 60 * SECOND;
-    public static final long YOUR = 60 * MINUTE;
-    public static final long DAY = 24 * YOUR;
-}
