6d451c9e860b02384e115b127119b8a1e17f0ef6
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-07-15 13:53:39 +0900

    #1 - Feat: Add User Constants

--- src/main/java/org/routemaster/api/auth/domain/user/impl/util/constant/UserRole.java
+++ src/main/java/org/routemaster/api/auth/domain/user/impl/util/constant/UserRole.java
@@ -1,6 +1,6 @@
-package org.routemaster.api.auth.domain.user.util.constant;
+package org.routemaster.api.auth.domain.user.impl.util.constant;
 
-public class UserRoleConstant {
+public class UserRole {
 
     public static final String ROLE_USER = "ROLE_USER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
